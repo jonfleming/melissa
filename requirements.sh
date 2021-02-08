172bf0475ce8cc2d1567b6848b7953d3b3a3c6cb
Jon Fleming <jonfleming@hotmail.com>
2021-02-08 02:37:24 +0000

    running as service on UpCloud

--- requirements.sh
+++ requirements.sh
@@ -4,13 +4,13 @@
 # conda activate chatterbot
 pip install chatterbot==1.0.2
 pip install chatterbot-corpus
-pip install spacy
-pip install django
-pip install numpy==1.19.2
-pip install neo4j==4.1.1
-pip install sphinx
-pip install recommonmark
-pip install django-docs
+conda install -c conda-forge spacy
+conda install django
+conda install numpy==1.19.2
+conda install neo4j==4.1.1
+conda install sphinx
+conda install recommonmark
+conda install django-docs
 python -m spacy download en
 # python -m spacy download en_core_web_md
 # python -m spacy download en_core_web_lg
---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# This script is run via anaconda python
+/opt/miniconda3/envs/melissa/bin/python manage.py runserver 0.0.0.0:8000
